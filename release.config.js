623edb2b8ccb50c53019e1e485ba80aac5e0861a
NunesClement <nunes.clement19@gmail.com>
2023-11-29 17:12:03 +0100

    test

--- release.config.js
+++ release.config.js
@@ -1,20 +1,20 @@
 const config = {
     branches: ['release'],
+    // plugins: [
+    //     '@semantic-release/commit-analyzer',
+    //     ['@semantic-release/git', {
+    //         "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
+    //         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    //     }],
+    //     '@semantic-release/github'
+    // ]
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        [
-            "@semantic-release/changelog",
-            {
-              "changelogFile": "docs/CHANGELOG.md"
-            }
-          ],
-        ['@semantic-release/git', {
-            "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }],
-        '@semantic-release/github'
-    ]
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/git",
+        "@semantic-release/github"
+      ]
 };
 
 module.exports = config;
