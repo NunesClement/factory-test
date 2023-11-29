156d85a618547434dd42fa93020809f33f2d9c2c
Clément Nunes <nunes.clement19@gmail.com>
2023-11-29 20:48:44 +0100

    fix: testing (#15)
    * fix: test
    
    * test

--- release.config.js
+++ release.config.js
@@ -12,7 +12,17 @@ const config = {
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         "@semantic-release/changelog",
-        "@semantic-release/git",
+        [
+          "@semantic-release/git",
+          {
+            "assets": [
+              "docs/CHANGELOG.md",
+              "package.json",
+              "package-lock.json"
+            ],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+          }
+        ],
         "@semantic-release/github"
       ]
 };
